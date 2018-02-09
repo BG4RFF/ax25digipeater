f39671882800f95525a20707a5b79e5ff9c24f6e
Josef Matondang <josstemat@gmail.com>
2018-02-09 09:01:07 +0700

    added more description for further development

--- axdigi.c
+++ axdigi.c
@@ -31,14 +31,6 @@
 #define buffer_size 1500
 #define ssid_mask 0x1e
 
-/* Nama fungsi: check_digipeater_address
- * Argumen:
- * 1. unsigned char *packet_buffer : pointer yang menunjukkan lokasi memory
- *    posisi dari elemen pertama dari string paket yang diterima
- * 2. int packet_buffer_size : panjang string paket yang diterima
- * 3. unsigned char *port : pointer dari port socket yang menerima paket
- */
-
 int socket_file_descriptor;
 struct sockaddr socket_address;
 int address_size = sizeof(socket_address);
@@ -46,11 +38,28 @@ int packet_size;
 struct ifreq interface_request;
 int proto = ETH_P_AX25; // ETH_P_AX25 is defined on netax25/ax25.h
 
-void print_call(unsigned char *bptr){
-	printf("%c%c%c%c%c%c-%d", bptr[0] >> 1, bptr[1] >> 1,
-			bptr[2] >> 1, bptr[3] >> 1, bptr[4] >> 1, bptr[5] >> 1,
-			(bptr[6] >> 1) & 0xf);
+/*
+ * Nama fungsi: print_call
+ * Argumen:
+ * 1. unsigned char *call_sign : pointer yang menunjukkan posisi pertama dari
+ *    byte callsign ax.25
+ */
+
+void print_call(unsigned char *call_sign){
+	printf("%c%c%c%c%c%c-%d", call_sign[0] >> 1, call_sign[1] >> 1,
+			call_sign[2] >> 1, call_sign[3] >> 1, call_sign[4] >> 1, call_sign[5] >> 1,
+			(call_sign[6] >> 1) & 0xf);
 }
+
+/*
+ * Nama fungsi: change_digipeat_bit
+ * Argumen:
+ * 1. unsigned char *packet_buffer : pointer yang menunjukkan lokasi memory
+ *    elemen pertama dari string paket yang diterima
+ * 2. int packet_buffer_size : panjang string paket yang diterima
+ * 3. int digipeater_address_number : urutan digipeater address yang sesuai
+ *    callsign port
+ */
 
 void change_digipeat_bit(unsigned char *packet_buffer, int packet_buffer_size, int digipeater_address_number) {
   unsigned char *byte_pointer;
@@ -67,6 +76,14 @@ void change_digipeat_bit(unsigned char *packet_buffer, int packet_buffer_size, i
 
   byte_pointer[6] |= has_been_repeated_bit;
 }
+
+/* Nama fungsi: check_digipeater_address
+ * Argumen:
+ * 1. unsigned char *packet_buffer : pointer yang menunjukkan lokasi memory
+ *    elemen pertama dari string paket yang diterima
+ * 2. int packet_buffer_size : panjang string paket yang diterima
+ * 3. unsigned char *port : pointer dari port socket yang menerima paket
+ */
 
 int check_digipeater_address(unsigned char *packet_buffer, int packet_buffer_size, unsigned char *port_callsign){
   unsigned char *byte_pointer;
@@ -89,7 +106,7 @@ int check_digipeater_address(unsigned char *packet_buffer, int packet_buffer_siz
       continue;
     }
     if ((bcmp(byte_pointer, port_callsign, 6) == 0) && ((byte_pointer[6] & ssid_mask) == (port_callsign[6] & ssid_mask))) {
-      printf("Found our callsign in a digipeater address\n");
+      // printf("Found our callsign in a digipeater address\n");
       // printf("Byte_pointer = %.7s and Socket_address = %.7s", byte_pointer, interface_request.ifr_hwaddr.sa_data);
       // printf("Packet digipeater address: ");
       // print_call(byte_pointer);
@@ -98,7 +115,7 @@ int check_digipeater_address(unsigned char *packet_buffer, int packet_buffer_siz
       return digipeater_count;
     }
     else {
-      printf("Found a digipeater address that is not ours\n");
+      // printf("Found a digipeater address that is not ours\n");
       return -1;
     }
   }
@@ -143,12 +160,18 @@ int main(int argc, char *argv[]) {
     if (interface_request.ifr_hwaddr.sa_family == AF_AX25) {
       digipeater_address_number = check_digipeater_address(buffer, buffer_size, interface_request.ifr_hwaddr.sa_data);
       if (digipeater_address_number == -1){
-        printf("Got a packet without digipeater\n");
+        printf("Our digipeater callsign: ");
+        print_call(interface_request.ifr_hwaddr.sa_data);
+        printf("was not found in the next digi hop in the packet\n");
         continue;
       }
       change_digipeat_bit(buffer, buffer_size, digipeater_address_number);
       if (sendto(socket_file_descriptor, buffer, packet_size, 0, &socket_address, address_size) == -1){
         perror("sendto");
+        continue;
+      }
+      else {
+        printf("Packet has been digipeated successfully\n");
         continue;
       }
     }
